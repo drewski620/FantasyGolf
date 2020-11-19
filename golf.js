ba5f37135387c06bc222b466b4c7e9b2d3443ec6
Andrew Werner <drewski620@gmail.com>
2020-11-19 12:24:40 -0700

    on click

--- golf.js
+++ golf.js
@@ -28,6 +28,7 @@ function addScore() {
 // console.log(addScore())
 
 var generateBtn = document.querySelector("#generate")
+
 function writeScore() {
     var score = addScore();
     var scoreText = document.querySelector('#overallScore')
@@ -35,42 +36,4 @@ function writeScore() {
     scoreText.value = score;
 }
 
-generateBtn.addEventLisener('click', writeScore())
-// addScore();
-// console.log(addScore(1, darkHorse))
-
-
-
-
-
-
-
-
-
-
-// window.onload=function(){
-//     document.getElementById('form').addEventListener('submit', function totalPlayerScore(position)  {
-//         position.preventDefault()
-       
-        
-       
-//         document.getElementById('score').innerHTML = "This player's total score is " + score.innerHTML;
-//     });
-//   }
-
-//   var position = document.querySelector('[name="length"]').value;
-//   function totalPlayerScore(position) {
-//     if (position <= 30 && position > 20) {
-//         console.log(10 + 10)
-//     } else if (position <= 20 && position > 10) {
-//         console.log(10 + 20)
-//     } else if (position <= 10 && position > 5) {
-//         console.log(10 + 30)
-//     } else if (position <= 5 && position > 1) {
-//         console.log(10 + 40);
-//     } else if (position === 1) {
-//         console.log(10 + 50);
-//     } else {
-//         console.log(10)
-//     }
-// }
+generateBtn.addEventListener('click', writeScore)
