42a82cd866adad49f86a3de9f8d3e6702b39ed54
Andrew Werner <drewski620@gmail.com>
2020-12-18 12:11:47 -0700

    added resuable function

--- golf.js
+++ golf.js
@@ -1,3 +1,4 @@
+var playerName = document.getElementById("playerName");
 var yesBtn = document.getElementById("yes");
 var noBtn = document.getElementById("no");
 var positionEl = document.getElementById("position");
@@ -13,14 +14,14 @@ yesBtn.addEventListener("click", function (event) {
     event.preventDefault()
     totalScore = 10
     console.log(totalScore)
-    var response = "Your player's darkhorse points is at " + totalScore
+    var response = playerName.value + " is your Darkhorse and got you " + totalScore + " extra points."
     scoreEl.textContent = response;
 })
 
 noBtn.addEventListener("click", function (event) {
     event.preventDefault()
     totalScore = 0;
-    var response = "This player is not a dark horse " + totalScore
+    var response = playerName.value +  " is not a dark horse no additional points." 
     scoreEl.textContent = response;
 })
 
@@ -39,15 +40,39 @@ function addScore() {
     } else {
         totalScore += 0
     }
-    var total = "Your total score with this player is " + totalScore
+    var total = "Your total score with " + playerName.value + " is " + totalScore + " points."
     totalScoreEl.textContent = total
+
+    var list = document.createElement("LI")
+    var playerTotal = document.querySelector("#totalScore").lastChild
+    var t = document.createTextNode(playerName.value + ": " + totalScore + " points" )
+    list.appendChild(t);
+    document.getElementById("myList").appendChild(list)
+    
 }
+// function addPlayerList() {
+//     event.preventDefault();
+
+//     var list = document.createElement("LI")
+//     var playerTotal = document.querySelector("#totalScore").lastChild
+//     var t = document.createTextNode(playerName.value + ": " + totalScore + " points" )
+//     list.appendChild(t);
+//     document.getElementById("myList").appendChild(list)
+    
+// }
 
 function clearScore() {
     event.preventDefault();
+    
     var total = "You clear the score " + totalScore * 0;
     totalScoreEl.textContent = total
+    var response = "Your player's darkhorse points is at " + totalScore * 0;
+    scoreEl.textContent = response
+    playerName.value = ""
+    positionEl.value = ""
+   
 }
 
 totalBtn.addEventListener("click", addScore);
 clearBtn.addEventListener("click", clearScore);
+
